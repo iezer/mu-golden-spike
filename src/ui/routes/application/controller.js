6330f1a4e73d93b4f7c5c93201424aa66a444f35
Isaac Ezer <isaac.ezer@gmail.com>
2018-01-30 15:09:53 -0500

    Access services

--- src/ui/routes/application/controller.js
+++ src/ui/routes/application/controller.js
@@ -1,6 +1,10 @@
 import Controller from '@ember/controller';
+import  { inject as service } from '@ember/service'
 
 export default Controller.extend({
+  addonMain: service('mu-first-post'),
+  addonSecondary: service('mu-first-post::secondary'),
+
   actions: {
     sayHello() {
       alert('Hello from the future');
